779b3a808311282aa34bf1f8549a4ed76d4841fd
jjaram117 <61059935+jjaram117@users.noreply.github.com>
2021-03-23 17:25:50 -0400

    Create secrets.py

---
+++ secrets.py
@@ -0,0 +1,10 @@
+# This file is where you keep secret settings, passwords, and tokens!
+# If you put them in the code you risk committing that info or sharing it
+
+secrets = {
+    'ssid' : 'home ssid',
+    'password' : 'my password',
+    'timezone' : "America/New_York", # http://worldtimeapi.org/timezones
+    'aio_username': 'MY_ADAFRUIT_IO_USERNAME',
+    'aio_password': 'MY_ADAFRUIT_IO_PASSWORD'
+    }
